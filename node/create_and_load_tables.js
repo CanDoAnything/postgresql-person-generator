e7bba4ac9a046fd4fb386baa980341e210db9e77
Joseph Amalfitano <16958794+CanDoAnything@users.noreply.github.com>
2019-04-19 12:28:15 -0400

    node testing

--- node/create_and_load_tables.js
+++ node/create_and_load_tables.js
@@ -1,28 +1,39 @@
 var fs = require('fs');
 var path = require('path');
 
-const { Client } = require('pg');
+const pgp = require('pg-promise');
 
 var filePath = path.join(__dirname, '..', 'secrets.json');
 var secrets = JSON.parse(fs.readFileSync(filePath,'utf8'));
 
 
-const config = {
-    host: secrets.pgHost,
-    database: secrets.pgDatabase,
-    user: secrets.pgUsername,
-    password: secrets.pgPassword,
-    port:5432
-}
+// const cn = {
+//     host: secrets.pgHost,
+//     database: secrets.pgDatabase,
+//     user: secrets.pgUsername,
+//     password: secrets.pgPassword,
+//     port:5432
+// }
 
 
-const client = new Client(config)
+const db = pgp('postgres://127.0.0.1:5432/demo_db');
+
+db.any('SELECT COUNT(*) FROM person')
+.then(data=> {
+    console.log('Data:',data);
+})
+.catch(error => {
+    console.log("oopsie:",error);
+})
+.finally(db.$pool.end);
 
 
 client.connect()
-const res =  client.query('SELECT COUNT(*) FROM person')
-console.log(res.rows[0].message)
-client.end()
+// client.query('SELECT COUNT(*) FROM person')
+//     .then(res => console.log(res.rows[0]))
+//     .catch(e => console.error(e.stack))
+
+
 
 var filePath = path.join(__dirname, '..', 'scripts','drop_tables.sql');
 var sqlString = fs.readFileSync(filePath,'utf8');
